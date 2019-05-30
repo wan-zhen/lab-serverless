d32402451d520ba27c06b6dc11961cae676b9bbe
wanzhen <kelly811124811124@hotmail.com>
2019-05-30 14:33:17 +0800

    add function api

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,20 @@
 import * as functions from 'firebase-functions';
-
+import * as express from 'express';
+import * as bodyParser from 'body-parser';
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
 // export const helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+const app = express();
+const main = express();
+app.use('/api/v1', main);
+main.use(bodyParser.json());
+main.use(bodyParser.urlencoded({ extended: false }));
+export const webApi = functions.https.onRequest(main);
+app.get('/demo', (req, res) => {
+  res.status(200).send({
+    result: [1, 2, 3]
+  });
+});
